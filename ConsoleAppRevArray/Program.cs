3a6bb012385f41dc6f7722e5f7d7e2f657b583b2
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-30 16:00:18 +0530

    Add project files.

---
+++ ConsoleAppRevArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DemoRevArray
+{
+    class RevArray
+    {
+        public void Rev(int[] arr)
+        {
+            Console.WriteLine("The Original Array");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            Console.WriteLine("The Reverse Array");
+            for (int i = arr.Length - 1; i >= 0; --i)
+
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RevArray r1 = new RevArray();
+            int[] arr = new int[] { 2, 4, 6, 8, 10, 12, 14 };
+            r1.Rev(arr);
+            Console.ReadKey();
+        }
+    }
+}
